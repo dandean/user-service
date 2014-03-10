9bbbe6a3b2f505f06fe45b2830d73c68a549826c
Dan Dean <dan.dean@simple.com>
2014-03-10 12:39:29 -0700

    Logging
    Fixes #11

--- index.js
+++ index.js
@@ -19,13 +19,42 @@ var Sequelize = require('sequelize');
 var config = JSON.parse(fs.readFileSync('./config/config.json'))['development'];
 
 //
+// CREATE AND CONFIGURE LOGGER
+//
+
+var Bunyan = require('bunyan');
+var log = new Bunyan({
+  name: 'UserService',
+  streams: [
+    {
+      stream: process.stdout,
+      level: 'debug'
+    },
+    {
+      type: 'rotating-file',
+      path: 'logs/trace.log',
+      level: 'trace',
+      period: '1d',           // Daily rotation
+      count: 3                // Keep 3 back copies
+    }
+  ],
+  serializers: {
+    req: Bunyan.stdSerializers.req,
+    res: restify.bunyan.serializers.res,
+  },
+});
+
+//
 // DATABASE CONNECTION
 //
 
 var sequelize = new Sequelize(config.database, config.username, config.password, {
   dialect: 'postgres',
   host: config.host,
-  port: 5432
+  port: 5432,
+  logging: function(message) {
+    log.info('PSQL: %s', message);
+  }
 });
 
 // Load the user model:
@@ -37,7 +66,8 @@ var User = sequelize.import(__dirname + "/lib/models/user");
 
 var server = restify.createServer({
   name: 'UserService',
-  version: '0.0.0'
+  version: '0.0.0',
+  log: log
 });
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
@@ -53,6 +83,7 @@ Service.config = config;
 Service.Sequelize = Sequelize;
 Service.models.User = User;
 Service.server = server;
+Service.logger = log;
 
 //
 // HTTP RESOURCES
